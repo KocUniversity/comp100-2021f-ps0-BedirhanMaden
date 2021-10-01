731c3e267383b90657d5c9b5156771e65f29fcc4
BedirhanMaden <91727129+BedirhanMaden@users.noreply.github.com>
2021-10-01 10:05:39 +0000

    Final

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy
+
+firstnumber=input("Please enter a number:")
+secondnumber=input("Please enter an another number:")
+
+z = int(firstnumber) ** int(secondnumber)
+
+print("First Number**Second Number:",int(z))
+
+
+Logarithm = numpy.log2(int(firstnumber))
+
+print("Log(First Number):",Logarithm)
+
+print("76430")
